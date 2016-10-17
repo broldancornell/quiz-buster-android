f7c7d5961cd39aae600d30c15c31f5146f5b4703
Brian <brian@blacksoftware.co.nz>
2016-10-18 01:09:50 +1300

    Added game code validation

--- app/src/main/java/com/example/k/quizbuster/MainActivity.java
+++ app/src/main/java/com/example/k/quizbuster/MainActivity.java
@@ -1,59 +1,129 @@
 package com.example.k.quizbuster;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.k.quizbuster.utility.JsonHttpRequest;
+import com.example.k.quizbuster.utility.JsonHttpRequestCallback;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
-    Typeface fontStyle;  // Typeface for Font sytle
+    private final String serverAddress = "http://quizbuster.co.nf//service/buster/validate.php?game_code=";
 
-    EditText txtgame_code; //game code
-    Button   btnSendGameCode;  // button for sending game code
-    private static final String SERVER_ADDRESS = "http://localhost/service/buster/validate.php?game_code=";
+    private Typeface fontStyle;  // Typeface for Font style
+    private EditText editTextGameCode; //game code
+    private Button buttonSendGameCode;  // button for sending game code
+
+    private ProgressDialog progressDialog;
+
+    private String gameCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        txtgame_code    = (EditText) findViewById(R.id.edit_text_game_code);
-        btnSendGameCode = (Button)   findViewById(R.id.btnSendGameCode);
+        prepareWidgets();
 
-        fontStyle = Typeface.createFromAsset(getAssets(), "TimKid.ttf");
-        txtgame_code.setTypeface(fontStyle);
-        btnSendGameCode.setTypeface(fontStyle);
     }
 
-    public void enterGameCode(View v) {
-        String game_code = txtgame_code.getText().toString();
+    private void prepareWidgets(){
+        editTextGameCode = (EditText) findViewById(R.id.edit_text_game_code);
+        buttonSendGameCode = (Button)   findViewById(R.id.btnSendGameCode);
+        fontStyle = Typeface.createFromAsset(getAssets(), "TimKid.ttf");
+        editTextGameCode.setTypeface(fontStyle);
+        buttonSendGameCode.setTypeface(fontStyle);
 
+        this.progressDialog = new ProgressDialog(this);
+        this.progressDialog.setMessage("Please wait");
 
-        Toast.makeText(this, "Game Code : " + game_code, Toast.LENGTH_SHORT).show();  // to check game code
+    }
 
-        try {
-            URL url = new URL(SERVER_ADDRESS + URLEncoder.encode(game_code, "UTF-8"));  // make server address with game code
-            url.openStream();  // send game code to php
-        }catch(Exception e)
-        {
-            e.printStackTrace();
+    public void enterGameCode(View view) {
+        final String gameCode = editTextGameCode.getText().toString();
+
+        if(gameCode.isEmpty()) {
+            Toast.makeText(view.getContext(), "Please provide a game code", Toast.LENGTH_LONG).show();
+            return;
         }
 
-        Toast.makeText(this, "Game URL  : " + SERVER_ADDRESS + game_code, Toast.LENGTH_SHORT).show();
-        //To check URL by eyes
+        this.gameCode = gameCode;
+        this.progressDialog.show();
+
+        JsonHttpRequest request = new JsonHttpRequest(serverAddress + gameCode, new JsonHttpRequestCallback() {
+            @Override
+            public void onCompleted(JSONObject data) {
+                progressDialog.hide();
+                processValidationResult(data);
+            }
+
+            @Override
+            public void onError(String message) {
+                progressDialog.hide();
+                Log.e(this.getClass().getSimpleName(), message);
+            }
+        });
+
+        request.execute();
+    }
+
+    private void processValidationResult(JSONObject result){
+        if(result == null){
+            Log.e(this.getClass().getSimpleName(), "Result JSON is null.");
+            return;
+        }
+
+        try {
+            int status = result.getInt("status");
+            String statusMessage = result.getString("status_message");
+            if(status != 200){
+                Log.e(this.getClass().getSimpleName(), "HTTP result status indicated an error: " + statusMessage);
+                return;
+            }
+
+            //decrypt JSOn object
+            JSONObject data = new JSONObject(result.getString("data"));
+            if(data == null){
+                Log.e(this.getClass().getSimpleName(), "HTTP result did not include data object");
+                return;
+            }
+
+            boolean available = data.getBoolean("available");
+
+            if(available){
+                moveOnToNickNameActivity();
+            }else{
+                Toast.makeText(this, "Game code is invalid", Toast.LENGTH_LONG).show();
+            }
+
+        } catch (JSONException exception) {
+            Log.e(this.getClass().getSimpleName(), "JSON exception encountered", exception);
+            return;
+        }
+
+
+    }
+
+    private void moveOnToNickNameActivity(){
 
         Intent nicknameActivity = new Intent(this, NicknameActivity.class);
 
-        Toast.makeText(this,"Welcome to Quiz Buster ! " + game_code, Toast.LENGTH_SHORT).show();
-        nicknameActivity.putExtra("entered_quiz_code", game_code);
+        nicknameActivity.putExtra("entered_quiz_code", gameCode);
         this.startActivity(nicknameActivity);
     }
 }
