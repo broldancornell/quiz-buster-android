48b5a231f9603156466135792c47206368a9c9af
Brian <brian@blacksoftware.co.nz>
2016-10-21 08:35:27 +1300

    Fixed layout for question activity

--- app/src/main/java/com/example/k/quizbuster/QuestionActivity.java
+++ app/src/main/java/com/example/k/quizbuster/QuestionActivity.java
@@ -1,6 +1,5 @@
 package com.example.k.quizbuster;
 
-import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -11,62 +10,60 @@ import android.widget.Toast;
 
 public class QuestionActivity extends AppCompatActivity {
 
-    TextView    txtQuestion;
-    TextView    txtQuestionNumber;
-    TextView    txtTime;
-    TextView    txtTimeNumber;
+    private TextView textViewQuestion;
+    private TextView    txtQuestionNumber;
+    private TextView textViewTimeLabel;
+    private TextView textViewTimeValue;
 
-    ImageButton btnAnswer1;
-    ImageButton btnAnswer2;
-    ImageButton btnAnswer3;
-    ImageButton btnAnswer4;
-    ImageButton btnShowResult;
-
-    Typeface fontStyle;
+    private ImageButton buttonAnswerOne;
+    private ImageButton buttonAnswerTwo;
+    private ImageButton buttonAnswerThree;
+    private ImageButton buttonAnswerFour;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question);
 
-        txtQuestion       = (TextView)    findViewById(R.id.txtQuestion);
-        txtQuestionNumber = (TextView)    findViewById(R.id.txtQuestionNumber);
-        txtTime           = (TextView)    findViewById(R.id.txtTime);
-        txtTimeNumber     = (TextView)    findViewById(R.id.txtTimeNumber);
-
-        btnAnswer1        = (ImageButton) findViewById(R.id.btnAnswer1);
-        btnAnswer2        = (ImageButton) findViewById(R.id.btnAnswer2);
-        btnAnswer3        = (ImageButton) findViewById(R.id.btnAnswer3);
-        btnAnswer4        = (ImageButton) findViewById(R.id.btnAnswer4);
-        btnShowResult     = (ImageButton) findViewById(R.id.btnShowResult);
-
-        fontStyle = Typeface.createFromAsset(getAssets(), "TimKid.ttf");
-
-        txtQuestion.setTypeface(fontStyle);
-        txtQuestionNumber.setTypeface(fontStyle);
-        txtTime.setTypeface(fontStyle);
-        txtTimeNumber.setTypeface(fontStyle);
+        prepareWidgets();
+        
     }
 
-    public void onBtnAnswer1(View v) {
-        Toast.makeText(this,"Answer 1 Clicked", Toast.LENGTH_SHORT).show();
+    private void prepareWidgets(){
+        textViewQuestion = (TextView) findViewById(R.id.text_view_question);
+        textViewTimeLabel = (TextView) findViewById(R.id.text_view_time_label);
+        textViewTimeValue = (TextView) findViewById(R.id.text_view_time_value);
+
+        buttonAnswerOne = (ImageButton) findViewById(R.id.button_answer_1);
+        buttonAnswerTwo = (ImageButton) findViewById(R.id.button_answer_2);
+        buttonAnswerThree = (ImageButton) findViewById(R.id.button_answer_3);
+        buttonAnswerFour = (ImageButton) findViewById(R.id.button_answer_4);
+
+        Typeface fontStyle = Typeface.createFromAsset(getAssets(), "TimKid.ttf");
+
+        textViewQuestion.setTypeface(fontStyle);
+        textViewTimeLabel.setTypeface(fontStyle);
+        textViewTimeValue.setTypeface(fontStyle);
     }
 
-    public void onBtnAnswer2(View v) {
-        Toast.makeText(this,"Answer 2 Clicked", Toast.LENGTH_SHORT).show();
+    public void onBtnAnswer1(View view) {
+        answerSelected(1);
     }
 
-    public void onBtnAnswer3(View v) {
-        Toast.makeText(this,"Answer 3 Clicked", Toast.LENGTH_SHORT).show();
+    public void onBtnAnswer2(View view) {
+        answerSelected(2);
     }
 
-    public void onBtnAnswer4(View v) {
-        Toast.makeText(this,"Answer 4 Clicked", Toast.LENGTH_SHORT).show();
+    public void onBtnAnswer3(View view) {
+        answerSelected(3);
     }
 
-    public void onResultClicked(View v) {
-        Intent resultActivity = new Intent(this, ResultActivity.class);
-        this.startActivity(resultActivity);
+    public void onBtnAnswer4(View view) {
+        answerSelected(4);
+    }
+
+    private void answerSelected(int answerNumber){
+        Toast.makeText(this,"Answer " + answerNumber + " Clicked", Toast.LENGTH_SHORT).show();
     }
 
 }
